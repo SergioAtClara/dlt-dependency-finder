c06a011d291ec4a4e649740f9a96ac2162e33210
Sergio Piza <sergio.carrazzoni@clara.team>
2024-06-13 10:40:56 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# dlt-dependency-finder
+# DLT Dependency Finder
+
+DLT Dependency Finder is a utility script designed to search for dependencies in DLT mart models present in [data-analytics-dbt repository](https://github.com/clara-team/data-analytics-dbt/). This script simplifies the process of identifying dependencies within marts.
+
+## Usage
+
+1. **Set the `base_path` and `mart_to_search` variables** in the `dlt_dep_find.sh` script:
+
+   - `base_path`: This should be set to the path where you cloned the `data-analytics-dbt` repository.
+   - `mart_to_search`: Set this to the name of the mart to search (without the `.sql` extension). You can also set `mart_to_search="*"` to search all marts in the `marts_dir`.
+
+2. **Run the script**:
+
+   ```bash
+   bash ./dlt_dep_find.sh
+   ```
---
+++ dlt_dep_find.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+: ' This script searches for dependencies in DLT models.
+    It searches for dependencies in the models in the marts directory and the models in the
+     directories specified in dep_directories.
+    It then prints the source files for the mart being searched and the dependencies found in
+     other models.
+    ** Please notice that the script might duplicate dependencies if the same dependency is found in
+     multiple models. You can use the python code list(dict.fromkeys(mylist)) to remove duplicates easily.
+    ** The script will also find all source files for dependencies that have the same name.'
+
+base_path="$HOME/projects/data-analytics-dbt"
+marts_dir="$base_path/models/marts"
+
+# Name of the mart to search (without the .sql extension)
+# You can replace this with * to search all marts in marts_dir (mart_to_search="*")
+mart_to_search="sat_invoicing_summary_origination"
+
+dep_directories=("$base_path/models/staging" "$base_path/models/intermediate")
+
+debug=0
+
+echo_path_without_base() {
+    path_without_sql_extension=$(echo "$1" | sed 's/.sql//')
+    echo "${path_without_sql_extension/$base_path\//}"
+}
+
+debug_echo() {
+    if [ $debug -eq 1 ]; then
+        echo "$1"
+    fi
+}
+
+# Function that will go through dep_directories and search for dependencies recursively
+dep_search() {
+    mart_dep_table_name="$1"
+
+    debug_echo "Searching dependencies for: $mart_dep_table_name"
+    for dir in "${dep_directories[@]}"; do
+        all_files=$(find "$dir" -type f -name "*.sql")
+
+        for model_file in $all_files; do
+            if grep -q -E "(CREATE TEMPORARY|CREATE OR REFRESH) LIVE (VIEW|TABLE) $1 AS" "$model_file"; then
+
+                debug_echo "found dependency $mart_dep_table_name in $model_file"
+                echo_path_without_base "    \"$model_file\","
+                model_dep_table_names=$(grep -o 'LIVE\.[a-zA-Z0-9_]*' "$model_file" | sed 's/LIVE\.//')
+
+                for model_dep_table_name in $model_dep_table_names; do
+                    dep_search "$model_dep_table_name"
+                done
+
+            fi
+        done
+
+    done
+}
+
+# Loop through each SQL model file in the directory
+mart_files=$(find "$marts_dir" -type f -name "$mart_to_search.sql")
+for mart_file in "${mart_files[@]}"; do
+    # echo "Dependencies for $marts_dir:"
+    echo ""
+    echo "Source files for $mart_file:"
+
+    # Extract table names from the model file
+    mart_dep_table_names=$(grep -o 'LIVE\.[a-zA-Z0-9_]*' "$mart_file" | sed 's/LIVE\.//')
+
+    # Loop through each table name and search for dependencies in other model files
+    for mart_dep_table_name in $mart_dep_table_names; do
+        dep_search "$mart_dep_table_name"
+    done
+
+    echo_path_without_base "    \"$mart_file\""
+done
